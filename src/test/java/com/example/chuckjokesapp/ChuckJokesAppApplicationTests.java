64b5bb8af4c59d8c31fc8cefb5730ddbffe9cf04
Dmitrii Bukreev <1stringsamurai@gmail.com>
2022-03-23 12:09:45 +0100

    Initial commit

---
+++ src/test/java/com/example/chuckjokesapp/ChuckJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chuckjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
